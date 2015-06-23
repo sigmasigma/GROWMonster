2577ee7ad40abead17c81e2efc0025d5bf133df7
gushimakota <gushi_g.and.v@i.softbank.jp>
2015-06-24 00:23:25 +0900

    時間取得するとこまで出来た

--- app/src/main/java/com/example/gushimakota/growmonster/MainActivity.java
+++ app/src/main/java/com/example/gushimakota/growmonster/MainActivity.java
@@ -14,30 +14,64 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.content.Context;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 public class MainActivity extends ActionBarActivity {
+    Calendar calendar_now,c1;
     ImageView monster;
+    TextView text;
     Bitmap bitImage;
     Resources resM;
-    int hungry=3,muscle=0,tired=0,stress=0;
-
-
+    int hungry,muscle,tired,stress,year,day,hour,min;
+    long time;
+    SharedPreferences prefer;
+    Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         monster=(ImageView)findViewById(R.id.monster);
+
         resM = getResources();
-        SharedPreferences prefer = getSharedPreferences("Ref", MODE_PRIVATE);
-        Editor editor = prefer.edit();
-        editor.putInt("hungry",hungry);
-        editor.putInt("muscle",muscle);
-        editor.putInt("tired",tired);
-        editor.putInt("stress",stress);
-        editor.putInt("id",R.drawable.form1);
-        bitImage = BitmapFactory.decodeResource(resM, prefer.getInt("id", R.drawable.form1));
+        prefer = getSharedPreferences("Ref", MODE_PRIVATE);
+        editor = prefer.edit();
+
+        calendar_now = Calendar.getInstance();
+        year = calendar_now.get(Calendar.YEAR);
+        day = calendar_now.get(Calendar.DAY_OF_YEAR);
+        hour = calendar_now.get(Calendar.HOUR);
+        min = calendar_now.get(Calendar.MINUTE);
+        time = ((year - prefer.getInt("year",year))*8760 +
+                (day-prefer.getInt("day", day))*24+
+                (hour - prefer.getInt("hour", hour)))*60 +
+                (min-prefer.getInt("min",min));
+
+
+        hungry = prefer.getInt("hungry",3);
+        muscle = prefer.getInt("muscle",3);
+        tired = prefer.getInt("tired", 3);
+        stress = prefer.getInt("stress",3);
+        if(hungry>=20) {
+            editor.putInt("mons_id", R.drawable.form2);
+            editor.commit();
+        }else if(hungry>=10){
+            editor.putInt("mons_id", R.drawable.form3);
+            editor.commit();
+        }else if(hungry>=3){
+            editor.putInt("mons_id", R.drawable.form1);
+            editor.commit();
+        }else{
+            editor.putInt("mons_id", R.drawable.form4);
+            editor.commit();
+        }
+        bitImage = BitmapFactory.decodeResource(resM, prefer.getInt("mons_id", R.drawable.form1));
         monster.setImageDrawable(null);
         monster.setImageBitmap(null);
         monster.setImageBitmap(bitImage);
@@ -48,7 +82,11 @@ public class MainActivity extends ActionBarActivity {
         if(hungry>10){
             stress++;
         }
-
+        editor = prefer.edit();
+        editor.putInt("hungry",hungry);
+        editor.putInt("stress", stress);
+        editor.commit();
+        //text.setText(String.valueOf(prefer.getInt("hungry",-1)));
 //        resM = getResources();
 //        if(bitImage!=null){
 //            bitImage.recycle();
@@ -61,6 +99,16 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void sport(View v){
+        hungry-=3;
+        if(hungry>10){
+            stress+=2;
+        }
+        editor = prefer.edit();
+        editor.putInt("hungry",hungry);
+        editor.putInt("stress", stress);
+        editor.commit();
+        //text.setText(String.valueOf(prefer.getInt("hungry",-1)));
+
     }
 
     @Override
